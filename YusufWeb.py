09ff1c39d1b4018cda520adeb0edc87f0517d83f
YusufBeraSERT <zesert2141@gmail.com>
2024-09-04 23:57:10 +0300

    Create YusufWeb.py

---
+++ YusufWeb.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teknoloji bağımlılığı nedir?</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>YusufWeb</h1>
+    <h1>Teknoloji Bağımlılığı Nedir?</h1>
+    <p>Teknoloji bağımlılığı, akıllı telefonun ortaya çıkışıyla kişinin sosyal medyada, oyunlarda, online alışveris sitelerinde ve uygulamalarda bilinçsiz, kontrolsüz ve takıntılı bir şekilde zaman harcamasıdır. Bu zaman harcama kişiyi aynı zamanda sosyal hayattan da soyutlar.
+
+        Teknoloji bağımlılığı ile mücadele eden insan sayısında özellikle internet ve akıllı telefonların ortaya çıkışı ile birlikte ciddi bir artış yaşanmıştır. Bireyler çevrimiçi iken beyinlerinde gelişen nörolojik değişiklikler göz önüne alındığında teknoloji bağımlılığı davranışsal bağımlılıklar başlığı altında incelenir. Davranışsal bağımlılıklar başlığı altında incelenen diğer yaygın alışkanlıklar kumar ve seks bağımlılıklarıdır. 
+        
+        Teknoloji bağımlılığı aynı zamanda dijital bağımlılık, internet bağımlılığı ya da internet kullanım bozukluğu olarak da adlandırılır. Teknoloji bağımlılığının, çocukların gelişimine zarar vermemesi adına profesyonel bir destek alınarak gelişim sürecinde dengeli bir role sahip olmalıdır.</p>
+    
+             </ul>
+     <img src="https://adiyaman.edu.tr/files/adiyaman.edu.tr/tr/haberler/2021/12/02/teknoloji-bagimliligi-gelecek-adina-kaygiya-neden-oluyor/84347.jpg" alt="Image1">
+     <img src="https://www.4ubilisim.com/wp-content/uploads/2021/07/teknoloji-bagimliligi.jpg">
+
+</html>
+<p>Teknoloji bağımlılığı, başta sosyal medya ve oyun, online alışveriş ve uygulamalarda takıntılı bir şekilde vakit geçirmek ve buna bağlı olarak kişinin kendini sosyal hayattan soyutlamasıdır. Modern çağın en yaygın sorunlarından biri olan teknoloji bağımlılığı özellikle çocuklar arasında hızla yayılmaktadır.</p>
+<h2>Teknoloji bağımlılığını önlemek için ne yapmalıyız?</h2>
+<p>Çocukların fiziksel aktivite ve teknolojik aletler dışında aile ile birlikte zaman geçirmesi, etkinlikler yapması teknoloji bağımlılığını önleyicidir. Çocukların sosyal ilişkiler kurması ve aile ve arkadaşlarıyla yeterince zaman geçirmesi teknoloji bağımlılığını önüne geçebilir.</p>
+    <img src="https://www.gulaysavas.com.tr/wp-content/uploads/2022/06/teknolojibagimliligi9.jpeg">
+<h2>Teknoloji bağımlılığını önlemek için ne yapmalıyız?</h2>
+<p>Çocukların fiziksel aktivite ve teknolojik aletler dışında aile ile birlikte zaman geçirmesi, etkinlikler yapması teknoloji bağımlılığını önleyicidir. Çocukların sosyal ilişkiler kurması ve aile ve arkadaşlarıyla yeterince zaman geçirmesi teknoloji bağımlılığını önüne geçebilir.</p>
+<h2>filmler</h2>
+<video src="video.mp4"
+width="600px"
+controls
+></video>
+
+<video src="videos.mp4"
+width="600px"
+controls
+></video>
+</body>
+
+<h2>TEKNOLOJİ BAĞIMLILIĞININ ETKİLERi</h2>
+<p>Baş, boyun ve sırt ağrıları
+   * Karpal tünel sendromu.
+   * Uykusuzluk (insomnia)
+   * Kilo artışı ve obezite.
+   * Kişisel bakım ve hijyenin ihmal edilmesi.
+   * Göz kuruluğu ve diğer görüş problemleri.
+   * Kalp-damar hastalıkları
+    </p>
