05f48afbfff290da0a7a91eef241223b45d8a376
YusufBeraSERT <zesert2141@gmail.com>
2024-09-04 23:58:20 +0300

    Create css.py

---
+++ css.py
@@ -0,0 +1,12 @@
+body {
+    font-family: Arial, sans-serif; /* Yazı tipi ailesi */
+    font-size: 21px; /* <p></p> etiketindeki ana yazının boyutu */
+    background-color: hwb(0 95% 5%); /* Web sayfasının arka plan rengi */
+    color: rgb(0, 0, 0); /* Metin rengi */
+}
+
+h1 {
+    color: hwb(0 0% 100%); /* Başlık rengi */
+    font-size: 24px; /* Başlık yazı tipi boyutu */
+    font-family: Georgia, serif; /* Başlıkta kullanılan yazı tipi ailesi */
+}
